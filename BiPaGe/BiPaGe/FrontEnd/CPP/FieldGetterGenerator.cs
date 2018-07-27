8e3fc218875e96182f45f36e11e634ff2bbd633e
Bouke <b.grevelt@gmail.com>
2018-07-27 18:01:10 +0200

    Consolidate different CreateBody methods I kept the visitor implementation for now even if it is useless at the moment because it is likely that we will need to deviate in the body generation again to add proper support for non-standard sized signed types, structures, strings and collections. But for now this is a nice cleanup.

--- BiPaGe/BiPaGe/FrontEnd/CPP/FieldGetterGenerator.cs
+++ BiPaGe/BiPaGe/FrontEnd/CPP/FieldGetterGenerator.cs
@@ -1,9 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Diagnostics;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
 using BiPaGe.Model;
 using BiPaGe.Model.FieldTypes;
 
@@ -42,77 +39,34 @@ namespace BiPaGe.FrontEnd.CPP
             bool needs_mask = (field.ByteAlginedOfffset != field.Offset) || (capture_size != field.Size);
             bool needs_shift = field.Shift != 0;
 
-            if (needs_mask || needs_shift)
-            {
-                // Return by value
+            if (needs_mask || needs_shift) // Return by value
                 declaration.Add($"{return_type} {field.Name}() const");
-            }
-            else
-            {
-                // Return by reference
+            else // Return by reference
                 declaration.Add($"const {return_type}& {field.Name}() const");
-            }
         }
 
-        public void CreateEnumerationBody()
+        private void CreateBody()
         {
-            String return_type = fieleTypeConverter.Convert(this.field.Type);
+            var return_type = field.CppType;
             String to_return = "*" + AddOffsetLine("data_offset", field.ByteAlginedOfffset, field.OffsetField);
-
-            bool needs_capture = field.CaptureType != "std::uint8_t";
-            if(needs_capture)
-                to_return = "*" + AddCaptureLine("captured_data", field.CaptureType);
-
-            var capture_size = field.CaptureSize;
-
-            if (field.NeedsMasking)
-            {
-                to_return = AddMaskLine("masked_data", field.CaptureType, to_return, field.Mask, field.Shift);
-                to_return = AddStaticCast(to_return, "typed_data", return_type);
-            }
-
-            //if (field.ByteAlginedOfffset == this.field.Offset && capture_size == this.field.Size)
-            //{
-            //    // We can simply cast directly to the return type
-            //    //to_return = "*" + AddCaptureLine("captured_data", return_type);
-            //}
-            //else
-            //{   
-            //    //bool needs_capture = field.CaptureType != "std::uint8_t";
-            //    //if (needs_capture)
-            //    //    to_return = "*" + AddCaptureLine("captured_data", field.CaptureType);
-             
-            //    // If the data we need is not byte algined or not one of C++'s standard data types, we will need to mask out the bits that we don't need
-            //    // If we do that we will also have to return by value instead of by reference. 
-            //    bool needs_mask = (field.ByteAlginedOfffset != field.Offset) || (capture_size != field.Size);
-            //    if (needs_mask)
-            //        to_return = AddMaskLine("masked_data", field.CaptureType, to_return, field.Mask, field.Shift);                    
-
-            //    to_return = AddStaticCast(to_return, "typed_data", return_type);               
-            //}
-            body.Add($"return {to_return};");            
-        }       
-
-        public void CreateFloatingPointBody()
-        {
-            String return_type = field.CppType;
-            String to_return = "*"+ AddOffsetLine("data_offset", field.ByteAlginedOfffset, field.OffsetField);
 
             bool needs_capture = field.CaptureType != "std::uint8_t";
             if (needs_capture)
                 to_return = "*" + AddCaptureLine("captured_data", field.CaptureType);
 
-            if (field.ByteAlginedOfffset == this.field.Offset)
-            {
-            //    to_return = "*" + AddCaptureLine("captured_data", return_type);                
-            }
-            else
-            {
-            //    AddCaptureLine("captured_data", field.CaptureType);
+            // If the data we need is not byte algined or not one of C++'s standard data types, we will need to mask out the bits that we don't need
+            // If we do that we will also have to return by value instead of by reference. 
+            if (field.NeedsMasking)
+                to_return = AddMaskLine("masked_data", field.CaptureType, to_return, field.Mask, field.Shift);
 
-                to_return = AddMaskLine("masked_data", field.CaptureType, "*captured_data", field.Mask, field.Shift);
+            // In some cases our capture data is larger than the data type we want to return (for example when a standard type is not byte algined). We removed all the data we don't need in the masking and shifting step
+            // so now cast back to the type we want to return
+
+            bool needs_type_cast = field.CaptureType != return_type;
+            if (needs_type_cast)
                 to_return = AddStaticCast(to_return, "typed_data", return_type);
-            }
+
+            // And finally add a line that returns the data
             body.Add($"return {to_return};");
         }
 
@@ -149,44 +103,6 @@ namespace BiPaGe.FrontEnd.CPP
             return to;
         }
 
-        public void CreateIntegralBody(String typeTemplate)
-        {
-            var return_type = field.CppType;
-            String to_return = "*" + AddOffsetLine("data_offset", field.ByteAlginedOfffset, field.OffsetField);            
-
-            // Add a line to create a pointer to the encapsulating data type (if that's not an uint8)
-            var capture_size = field.CaptureSize;
-
-            bool needs_capture = field.CaptureType != "std::uint8_t";
-            if(needs_capture)   
-                to_return = "*" + AddCaptureLine("captured_data", field.CaptureType);       
-
-            // If the data we need is not byte algined or not one of C++'s standard data types, we will need to mask out the bits that we don't need
-            // If we do that we will also have to return by value instead of by reference. 
-            if(field.NeedsMasking)            
-                to_return = AddMaskLine("masked_data", field.CaptureType, to_return, field.Mask, field.Shift);
-
-            // In some cases our capture data is larger than the data type we want to return (for example when a standard type is not byte algined). We removed all the data we don't need in the masking and shifting step
-            // so now cast back to the type we want to return
-            
-            bool needs_type_cast = field.CaptureType != return_type;
-            if(needs_type_cast)
-                to_return = AddStaticCast(to_return, "typed_data", return_type);
-
-            // And finally add a line that returns the data
-            body.Add($"return {to_return};");
-        }
-
-        public void CreateSignedIntegerBody()
-        {
-            CreateIntegralBody("std::int{0}_t");
-        }
-
-        public void CreateUnsignedIntegerBody()
-        {
-            CreateIntegralBody("std::uint{0}_t");
-        }
-
         public void Visit(AsciiString s)
         {
             throw new NotImplementedException();
@@ -205,19 +121,19 @@ namespace BiPaGe.FrontEnd.CPP
         public void Visit(FloatingPoint f)
         {
             CreateDeclaration();
-            CreateFloatingPointBody();
+            CreateBody();
         }
 
         public void Visit(SignedIntegral s)
         {
             CreateDeclaration();
-            CreateSignedIntegerBody();
+            CreateBody();
         }
 
         public void Visit(UnsignedIntegral u)
         {
             CreateDeclaration();
-            CreateUnsignedIntegerBody();
+            CreateBody();
         }
 
         public void Visit(Structure s)
@@ -228,7 +144,7 @@ namespace BiPaGe.FrontEnd.CPP
         public void Visit(Enumeration e)
         {
             CreateDeclaration();
-            CreateEnumerationBody();
+            CreateBody();
         }
     }
 }
