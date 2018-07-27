70694e9336862003d8001303c1ed67d60647b1b3
Bouke Grevelt <bgr@qps.nl>
2018-07-27 16:18:02 +0200

    Refactor: Consolidated all declaration generators to a single one.

--- BiPaGe/BiPaGe/FrontEnd/CPP/FieldGetterGenerator.cs
+++ BiPaGe/BiPaGe/FrontEnd/CPP/FieldGetterGenerator.cs
@@ -9,56 +9,6 @@ using BiPaGe.Model.FieldTypes;
 
 namespace BiPaGe.FrontEnd.CPP
 {
-    //class Field
-    //{
-    //    private Model.Field Original;
-    //    public Field(Model.Field original)
-    //    {
-    //        this.Original = original;
-    //        this.ByteAlginedOfffset = (original.Offset - (original.Offset % 8));
-    //        this.Mask = ComputeMask();
-    //        this.Shift = ComputeShift();
-    //        this.CaptureSize = ComputeCaptureSize();
-    //        this.CppType = buildCppType();
-    //    }
-    //    public String Name { get { return this.Original.Name; } }
-    //    public uint Size { get { return this.Original.SizeInBits(); } }
-    //    public uint Offset { get { return this.Original.Offset; } }
-    //    public uint ByteAlginedOfffset { get; }
-    //    public String OffsetField { get { return this.Original.OffsetFrom; } }
-    //    public Model.FieldType Type { get { return this.Original.Type; } }
-    //    public ulong Mask { get; }
-    //    public uint Shift { get; }
-    //    public uint CaptureSize { get; }
-    //    public String CppType { get; }
-
-    //    private ulong ComputeMask()
-    //    {
-    //        var mask = (ulong)Math.Pow(2, this.Size) - 1;
-    //        return mask << (int)(this.Offset - this.ByteAlginedOfffset);
-    //    }
-
-    //    private uint ComputeShift()
-    //    {
-    //        return this.Offset - this.ByteAlginedOfffset;
-    //    }
-
-    //    private uint ComputeCaptureSize()
-    //    {
-    //        var minimum_capture_size = this.Offset - this.ByteAlginedOfffset + this.Size;
-    //        var capture_type_width = (uint)Math.Max(Math.Pow(2, Math.Ceiling(Math.Log(minimum_capture_size) / Math.Log(2))), 8);
-    //        return capture_type_width;
-    //    }
-
-    //    private String buildCppType()
-    //    {
-    //        var cpp_size = (uint)Math.Max(8, Math.Pow(2, Math.Ceiling(Math.Log(this.Size) / Math.Log(2))));
-    //        Debug.Assert(cpp_size % 8 == 0);
-    //        return new FieldTypeConverter().Convert(this.Type, cpp_size);            
-    //    }
-    //}
-
-
     class FieldGetterGenerator : Model.IFieldTypeVisitor
     {
         private List<String> declaration = new List<string>();
@@ -84,18 +34,24 @@ namespace BiPaGe.FrontEnd.CPP
             return this.body;
         }
 
-        public void CreateEnumerationDeclaration()
+        private void CreateDeclaration()
         {
-            String return_type = fieleTypeConverter.Convert(this.field.Type);
+            String return_type = field.CppType;
             var capture_size = field.CaptureSize;
 
             bool needs_mask = (field.ByteAlginedOfffset != field.Offset) || (capture_size != field.Size);
             bool needs_shift = field.Shift != 0;
 
             if (needs_mask || needs_shift)
+            {
+                // Return by value
                 declaration.Add($"{return_type} {field.Name}() const");
+            }
             else
+            {
+                // Return by reference
                 declaration.Add($"const {return_type}& {field.Name}() const");
+            }
         }
 
         public void CreateEnumerationBody()
@@ -125,33 +81,11 @@ namespace BiPaGe.FrontEnd.CPP
                 to_return = AddStaticCast(to_return, "typed_data", return_type);               
             }
             body.Add($"return {to_return};");            
-        }
-
-        public void CreateFloatingPointDeclaration()
-        {
-            Debug.Assert(this.field.Size == 32 || this.field.Size == 64, "Only 32 bit and 64 bit floating point types are supported");
-            String return_type = this.field.Size == 32 ? "float" : "double";
-            var capture_size = field.CaptureSize;
-
-            bool needs_mask = (field.ByteAlginedOfffset != field.Offset) || (capture_size != field.Size);
-            bool needs_shift = field.Shift != 0;
-
-            if (needs_mask || needs_shift)
-            {
-                // Return by value
-                declaration.Add($"{return_type} {field.Name}() const");
-            }
-            else
-            {
-                // Return by reference
-                declaration.Add($"const {return_type}& {field.Name}() const");
-            }
-        }
+        }       
 
         public void CreateFloatingPointBody()
         {
-            Debug.Assert(this.field.Size == 32 || this.field.Size == 64, "Only 32 bit and 64 bit floating point types are supported");
-            String return_type = this.field.Size == 32 ? "float" : "double";
+            String return_type = field.CppType;
 
             // Determine offset to the data we need
             String to_return = "*"+ AddOffsetLine("data_offset", field.ByteAlginedOfffset, field.OffsetField);           
@@ -171,36 +105,6 @@ namespace BiPaGe.FrontEnd.CPP
                 to_return = AddStaticCast(to_return, "typed_data", return_type);
             }
             body.Add($"return {to_return};");
-        }
-
-        public void CreateIntegralDeclaration(String typeTemplate)
-        {
-            var return_type = field.CppType;
-            var capture_size = field.CaptureSize;
-
-            bool needs_mask = (field.ByteAlginedOfffset != field.Offset) || (capture_size != field.Size);
-            bool needs_shift = field.Shift != 0;
-
-            if (needs_mask || needs_shift)
-            {
-                // Return by value
-                declaration.Add($"{return_type} {field.Name}() const");
-            }
-            else
-            {
-                // Return by reference
-                declaration.Add($"const {return_type}& {field.Name}() const");
-            }
-        }
-
-        public void CreateSignedIntegerDeclaration()
-        {
-            CreateIntegralDeclaration("std::int{0}_t");
-        }
-
-        public void CreateUnsignedIntegerDeclaration()
-        {
-            CreateIntegralDeclaration("std::uint{0}_t");
         }
 
         private String AddOffsetLine(String variable_name, uint byte_aligned_offset, String offset_from)
@@ -292,19 +196,19 @@ namespace BiPaGe.FrontEnd.CPP
 
         public void Visit(FloatingPoint f)
         {
-            CreateFloatingPointDeclaration();
+            CreateDeclaration();
             CreateFloatingPointBody();
         }
 
         public void Visit(SignedIntegral s)
         {
-            CreateSignedIntegerDeclaration();
+            CreateDeclaration();
             CreateSignedIntegerBody();
         }
 
         public void Visit(UnsignedIntegral u)
         {
-            CreateUnsignedIntegerDeclaration();
+            CreateDeclaration();
             CreateUnsignedIntegerBody();
         }
 
@@ -315,7 +219,7 @@ namespace BiPaGe.FrontEnd.CPP
 
         public void Visit(Enumeration e)
         {
-            CreateEnumerationDeclaration();
+            CreateDeclaration();
             CreateEnumerationBody();
         }
     }
